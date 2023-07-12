42cc18365d0916937c55666e19074783d8de5feb
mehdii789 <mehdisamy95@gmail.com>
2023-07-12 11:57:04 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+// importe express
+const express = require('express');
+
+// déclaration de mongoose
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+// déclaration des routes et controleurs
+const sauceRoutes = require('./routes/stuff.js');
+const userRoutes = require('./routes/user');
+
+// utilisation du chemin et variables d'environnement
+const path = require('path');
+//const varEnv = require('dotenv').config();
+
+// créer une application express
+const app = express();
+app.use(express.json());
+
+// connexion à la base de donnée avec des variables d'environnement
+mongoose
+	.connect(`mongodb+srv://user1:1234@cluster0.ql2j3yc.mongodb.net/?retryWrites=true&w=majority`)
+	.then(() => console.log('Connexion à MongoDB réussie !'))
+	.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// middleware qui ne contient pas de route
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	next();
+});
+
+// routes
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+// route pour le dossier images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
---
+++ backend/models/Sauce.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  ingredient: { type: Number, required: true },
+  userId: { type: String, required: true },
+});
+
+module.exports = mongoose.model('sauce', sauceSchema);

e167f1d170db03e9b21c8961ef1c94b7a7de2dd2
mehdii789 <mehdisamy95@gmail.com>
2023-07-19 10:48:32 +0200

    v30

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const stuffRoutes = require('./routes/stuff');
+const stuffRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 

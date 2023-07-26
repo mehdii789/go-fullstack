9c9185e5920a4cb8d8c997941d03c25d924bfc9d
mehdii789 <mehdisamy95@gmail.com>
2023-07-26 11:26:08 +0200

    Like

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,11 +1,12 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
+const { findOne } = require('../models/Sauce');
 
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   delete sauceObject._userId;
-  const sauce = new sauce({
+  const sauce = new Sauce({
       ...sauceObject,
       userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -86,4 +87,98 @@ exports.getAllSauce = (req, res, next) => {
       });
     }
   );
+};
+
+// gère les Likes et Dislikes des sauces
+exports.likeAndDislike = (req, res, next) => {
+	const { like, userId } = req.body
+	if (![1, 0, -1].includes(like)) {
+		return res.status(403).send({ message: "Like/Dislike: Quantité invalide !" })
+	}
+	let likeString = like.toString();
+	switch (likeString) {
+		case '1': {
+			// met un Like
+			Sauce.updateOne(
+				{
+					// enregistre l'ID de l'utilisateur
+					_id: req.params.id, 
+				},
+				{
+					// Incrémente le champs "nombre d'utilisateurs qui ont mis un Like"
+					$inc: { likes: req.body.like++ }, 
+					// Enregistre l'ID de l'utilisateur dans le table de ceux qui ont aimés
+					$push: { usersLiked: req.body.userId }, 
+				}
+			)
+				.then((sauce) => res.status(200).json({ message: 'Like ajouté !' }))
+				.catch((error) => res.status(400).json({ error }));
+			break;
+		}
+
+		case '-1': {
+			// met un Dislike
+			Sauce.updateOne(
+				{
+					// enregistre l'ID de l'utilisateur
+					_id: req.params.id,
+				},
+				{
+					// Incrémente le champs "nombre d'utilisateurs qui ont mis un Dislike"
+					$inc: { dislikes: req.body.like++ * -1 },
+					// Enregistre l'ID de l'utilisateur dans le table de ceux qui n'ont aimés
+					$push: { usersDisliked: req.body.userId },
+				}
+			)
+				.then((sauce) => res.status(200).json({ message: 'Dislike ajouté !' }))
+				.catch((error) => res.status(400).json({ error }));
+			break;
+		}
+
+		default: {
+			// supprime un Like ou un Dislike
+			Sauce.findOne({ _id: req.params.id })
+				.then((sauce) => {
+					// test si le userId est dans le tableau des personnes qui ont liké la sauce
+					if (sauce.usersLiked.includes(req.body.userId)) {
+						Sauce.updateOne(
+							{
+								// enregistre l'ID de l'utilisateur
+								_id: req.params.id,
+							},
+							{
+								// enregistre l'ID de l'utilisateur dans le table de ceux qui ont aimés
+								$pull: { usersLiked: req.body.userId },
+								// supprime le Like
+								$inc: { likes: -1 },
+							}
+						)
+							.then((sauce) => {
+								res.status(200).json({ message: 'Like supprimé !' });
+							})
+							.catch((error) => res.status(400).json({ error }));
+					}
+					// test si le userId est dans le tableau des personnes qui ont Disliké la sauce
+					else if (sauce.usersDisliked.includes(req.body.userId)) {
+						Sauce.updateOne(
+							{
+								// enregistre l'ID de l'utilisateur
+								_id: req.params.id,
+							},
+							{
+								// enregistre l'ID de l'utilisateur dans le table de ceux qui n'ont aimés
+								$pull: { usersDisliked: req.body.userId },
+								// supprime le Dislike
+								$inc: { dislikes: -1 },
+							}
+						)
+							.then((sauce) => {
+								res.status(200).json({ message: 'Dislike supprimé !' });
+							})
+							.catch((error) => res.status(400).json({ error }));
+					}
+				})
+				.catch((error) => res.status(400).json({ error }));
+		}
+	}
 };
--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-  title: { type: String, required: true },
+  name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  ingredient: { type: String, required: true },
+  mainPepper: { type: String, required: true },
   userId: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+	dislikes: { type: Number, default: 0 },
+	usersLiked: { type: [String] },
+	usersDisliked: { type: [String] },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
